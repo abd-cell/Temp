37e6ba1489981df3ec318f6f7396f7922b022364
Abdalrhman Abdullah <abdalrhman.abdullah@technzone.com>
2022-10-06 15:20:21 +0300

    init commit

---
+++ Startup.cs
@@ -0,0 +1,50 @@
+﻿
+
+
+
+using Bookstore.Models;
+using Bookstore.Models.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookstore
+{
+    public class Startup
+    {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddScoped<IBookstoreRepository<Author>, AuthorDbRepository>();
+            services.AddScoped<IBookstoreRepository<Book>, BookDbRepository>();
+            services.AddDbContext<BookstoreDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DatabaseConnection"));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            app.UseMvc(route =>
+            {
+                route.MapRoute("default", "{controller=Book}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
